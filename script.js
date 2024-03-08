e244a48616a2e564460fd5178eddf4081c037295
Nic <pnkonx@gmail.com>
2024-03-07 22:08:47 -0700

    last before react switch

---
+++ script.js
@@ -0,0 +1,15 @@
+const liElements = document.querySelectorAll(".navigation ul li");
+
+liElements.forEach(liElement => {
+    liElement.addEventListener('click', () => {
+     document.querySelector('.active')?.classList.remove('active');
+        
+       liElement.classList.add('active');
+        
+
+       
+        
+
+    })
+})
+
