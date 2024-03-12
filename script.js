263d3394d90fc1be7d74b45f2bcbc27db7498e55
Nic <pnkonx@gmail.com>
2024-03-12 12:44:19 -0700

    last with llinks active

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const liElements = document.querySelectorAll(".navigation ul li");
-
-liElements.forEach(liElement => {
-    liElement.addEventListener('click', () => {
-     document.querySelector('.active')?.classList.remove('active');
-        
-       liElement.classList.add('active');
-       
-        
-
-       
-        
-
-    })
-})
 
